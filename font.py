cc28f44e7b6eb23d7dc7187ef8b6e5a2f08ad88e
prachi Garg <prachigarg092@gmail.com>
2025-12-29 15:24:17 +0530

    code

---
+++ font.py
@@ -0,0 +1,57 @@
+print("WELCOME TO FONT STYLE AI")
+print("------------------------")
+
+print("AVAILABLE FONTS: ")
+print("1.Serif Fonts (Classic & Elegant)")
+print("2.Sans-Serif Fonts (Modern & Clean)")
+print("3.Script & Handwritten Fonts (Stylish & Personal)")
+print("4.Display & Decorative Fonts (Fun & Unique)")
+
+choice = input("choose availabe fonts category(1-4):")
+if choice == "1":
+    print("Serif Fonts:")
+    print("1. Playfair Display")
+    print("2. Lora")
+    print("3. Alegreya")
+    print("4. Libre Baskerville")
+    print("5. Cinzel")
+
+elif choice == "2":
+    print("Sans-Serif Fonts:")
+    print("1. Montserrat")
+    print("2. Bebas Neue")
+    print("3. Helvetica / Arial")
+    print("4. Josefin Sans")
+    print("5. Overpass")
+    print("6. League Spartan")
+
+elif choice == "3":
+    print("Script & Handwritten Fonts:")
+    print("1. Parisienne")
+    print("2. Halo Handletter")
+    print("3. Yellowtail")
+    print("4. Cookie")
+    print("5. Lemon Tuesday")
+
+elif choice == "4":
+    print("Display & Decorative Fonts:")
+    print("1. Lilita One")
+    print("2. Fredoka One")
+    print("3. TAN Mignon")
+    print("4. TAN Astoria")
+
+else:
+    print("Invalid choice ❌")
+
+# font.py
+# All font mappings here
+
+#normal = "abcdefghijklmnopqrstuvwxyz"
+
+#bold = "𝐚𝐛𝐜𝐝𝐞𝐟𝐠𝐡𝐢𝐣𝐤𝐥𝐦𝐧𝐨𝐩𝐪𝐫𝐬𝐭𝐮𝐯𝐰𝐱𝐲𝐳"
+#circle = "ⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩ"
+
+#fonts = {
+ #   "bold": dict(zip(normal, bold)),
+  #  "circle": dict(zip(normal, circle))
+#}
